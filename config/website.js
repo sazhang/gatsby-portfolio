9aa70a898c649d4ae982c52556a68038fa2ba2ab
sazhang <zhang.sar@husky.neu.edu>
2019-04-26 16:16:58 -1000

    edited config

--- config/website.js
+++ config/website.js
@@ -1,20 +1,20 @@
 module.exports = {
   pathPrefix: '/',
-  title: "Minimal Blog", // Navigation and Site Title
-  titleAlt: "Minimal Blog - Gatsby Starter", // Alternative Site title for SEO
-  headline: "Writing and publishing content",
+  title: "Sarah Zhang - Dev & Design Portfolio", // Navigation and Site Title
+  titleAlt: "Sarah Zhang - T-Shaped Engineer", // Alternative Site title for SEO
+  headline: "Dev & Design Content",
   description:
-    "Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.",
+    "Projects of an engineer growing to be T-shaped",
   url: "https://sarahzhang.co",
   siteUrl: "https://sarahzhang.co",
   siteLanguage: "en",
   ogLanguage: "en_US",
-  logo: "/social/logo.png",
-  favicon: "src/favicon.png",
-  shortName: 'GatsbyTut',
+  logo: "src/assets/polyz.png",
+  favicon: "src/assets/polyz.png",
+  shortName: "Sarah Zhang",
   author: "Sarah Zhang",
-  themeColor: "#3498DB",
-  backgroundColor: "#2b2e3c",
+  themeColor: "#bac3f8",
+  backgroundColor: "#101C28",
   twitter: "@szhangster",
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   googleAnalyticsID: "UA-135465108-1",
